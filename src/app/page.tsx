08163bf41c6d599e87a4524811da2c2810b58c1e
sujal <sujalkesharwani220@gmail.com>
2025-03-29 01:52:36 +0530

    main page changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,16 @@ import Experience from "./(Experience)/Experience";
 export default function Home() {
   return (
     <>
-      <div className="h-screen w-screen bg-primary-foreground">
+      <div className="h-screen w-full bg-primary-foreground">
         <Navbar />
         <Herobody />
-        <AppNavbar />
+        <AppNavbar /> {/* Display only at mobile for bottom Navbar */}
       </div>
-      <div className="h-full md:h-[85vh] w-screen">
-        <About />
-      </div>
-      <div className="h-full w-screen">
-        <Skill />
-      </div>
-      <div className="h-full w-screen">
-        <Experience />
-      </div>
-      <div className="h-full w-screen">
-        <Project />
-      </div>
-      <div className="h-[80vh] md:h-full translate-y-24 w-screen">
-        <Contact />
-      </div>
+      <About />
+      <Skill />
+      <Experience />
+      <Project />
+      <Contact />
     </>
   );
 }
