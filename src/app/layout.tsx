0c4b84347800e9c182ede9c634e84e0ef60a83d3
sujal <sujalkesharwani220@gmail.com>
2025-03-28 20:21:22 +0530

    fixes overflow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,31 @@
-import { ThemeProvider } from '@/components/theme-provider'
-import './global.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Poppins } from 'next/font/google'
+import { ThemeProvider } from "@/components/theme-provider";
+import "./global.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const poppins = Poppins({subsets:['latin'],weight:['500']})
+const poppins = Poppins({ subsets: ["latin"], weight: ["500"] });
 
 export const metadata: Metadata = {
-  title: 'Sujal Portfolio',
-  description: 'Portfolio of Dhrish Parekh, A full stack developer and a curious mind',
-}
+  title: "Sujal Portfolio",
+  description: "Portfolio of Sujal, A full stack developer and a curious mind",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en " suppressHydrationWarning >
-      <body className={`${poppins.className} bg-primary-foreground`} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${poppins.className} bg-primary-foreground w-screen overflow-x-hidden`}
+        suppressHydrationWarning
+      >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         {children}
         </ThemeProvider>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
