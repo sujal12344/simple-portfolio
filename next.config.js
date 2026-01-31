8d4839e73bb5656028a996bf6b4aafb41854536d
sujal <sujalkesharwani220@gmail.com>
2026-02-01 00:05:41 +0530

    chore(add): qualities and format added

--- next.config.js
+++ next.config.js
@@ -1,29 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   eslint: {
-    ignoreDuringBuilds: true
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
       {
-        hostname: 'github.com',
-        protocol: 'https',
+        hostname: "github.com",
+        protocol: "https",
       },
       {
-        hostname: 'arkimals-game.com',
-        protocol: 'https',
+        hostname: "arkimals-game.com",
+        protocol: "https",
       },
       {
-        hostname: 'parivartanx.com',
-        protocol: 'https',
+        hostname: "parivartanx.com",
+        protocol: "https",
       },
       {
-        hostname: 'raffl.pro',
-        protocol: 'https',
-      }
-    ]
+        hostname: "raffl.pro",
+        protocol: "https",
+      },
+    ],
+    qualities: [75, 90, 100],
+    formats: ["image/avif", "image/webp"],
+  },
+};
 
-  }
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
